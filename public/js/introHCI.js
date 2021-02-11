a4d8606a95739e379ec603763a1b4e153d288443
Anand Suresh <aks037@ucsd.edu>
2021-02-11 10:54:54 -0800

    complete till slide 46

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
 function initializePage() {
 	$('.project a').click(addProjectDetails);
 
+
 	// $('#colorBtn').click(randomizeColors);
 }
 
@@ -25,6 +26,10 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	$.get("http://localhost:3000/project/" + idNumber, callBackFn);
 	console.log("User clicked on project " + idNumber);
+}
+
+function callBackFn(result) {
+	console.log(response);
 }
